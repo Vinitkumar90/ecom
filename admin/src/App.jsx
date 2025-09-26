510bd8108900b5046aff3036373d9b16ae645524
vinit <vinitkumar93341@gmail.com>
2025-09-26 23:54:38 +0530

    admin login page and routes

---
+++ admin/src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Orders from "./pages/Orders";
+import Login from "./components/Login";
+
+const App = () => {
+  const [token, setToken] = useState("");
+
+  return (
+    <BrowserRouter>
+      <div className="bg-gray-50 min-h-screen">
+        {token === "" ? (
+          <Login />
+        ) : (
+          <>
+            <Navbar />
+            <hr />
+            <div className="flex w-full">
+              <Sidebar />
+              <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+                <Routes>
+                  <Route path="/add" element={<Add />} />
+                  <Route path="/list" element={<List />} />
+                  <Route path="/orders" element={<Orders />} />
+                </Routes>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
