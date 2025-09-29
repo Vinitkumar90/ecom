e89989867ea7f18aeae513863e3d77b78d637054
vinit <vinitkumar93341@gmail.com>
2025-09-29 12:41:23 +0530

    order model and router

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,43 @@
+
+
+// placing order using COD
+const placeOrder = async (req,res) => {
+
+}
+
+
+// placing order using Stripe
+const placeOrderStripe = async (req,res) => {
+    
+}
+
+
+// placing order using Razorpay 
+const placeOrderRazorpay = async (req,res) => {
+    
+}
+
+//All orders data for admin panel
+const allOrders = async(req,res)=>{
+
+}
+
+//user orer data for front - end
+const userOrders = async(req,res) => {
+
+}
+
+//update order status from Admin panel
+const updateStatus = async(req,res) => {
+
+}
+
+
+export{
+    placeOrder,
+    placeOrderStripe,
+    placeOrderRazorpay,
+    allOrders,
+    userOrders,
+    updateStatus
+}
